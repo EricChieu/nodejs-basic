58bc929eaec80b5dac28eda0aea472a848c84bf1
EricChieu <chieuhieu97@gmail.com>
2025-08-07 23:23:36 +0700

    Fix port for deployment

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database"); // Nạp hàm kết nối
 const app = express();
-const port = 3000;
 
 // Kết nối đến cơ sở dữ liệu
 connectDB();
@@ -19,6 +18,9 @@ app.use("/api/users", userRoutes); // Đổi thành /api/users cho nhất quán
 
 app.use("/api/auth", authRoutes); // Sử dụng auth route
 
-app.listen(port, () => {
-  console.log(`Ứng dụng đang chạy tại http://localhost:${port}`);
+// Thay vì const port = 3000;
+const PORT = process.env.PORT || 3000; // Dùng cổng của server, nếu không có thì dùng 3000
+
+app.listen(PORT, () => {
+  console.log(`Ứng dụng đang chạy tại cổng ${PORT}`);
 });
